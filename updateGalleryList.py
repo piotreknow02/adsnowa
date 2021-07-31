a8208789b20e0f551f0eca898fa269e6d9fff704
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-07-31 14:23:18 +0200

    Gallery generator update

--- updateGalleryList.py
+++ updateGalleryList.py
@@ -3,7 +3,12 @@ import json
 
 # gallery
 file_list = os.listdir("./static/gallery")
-file_list.remove(".DS_Store")
+if not os.path.exists("./src/node_modules/"):
+	os.mkdir("./src/node_modules/")
+try:
+	file_list.remove(".DS_Store")
+except:
+	pass
 json_list = json.dumps(file_list)
 with open("./src/node_modules/galleryList.js", "w") as file:
 	file.write("const galleryList = " + json_list + "\nmodule.exports = { galleryList }")
